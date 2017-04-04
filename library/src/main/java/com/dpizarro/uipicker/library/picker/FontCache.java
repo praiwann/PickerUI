5843110257853921db1cdc29f09010f06b275fd1
Unknown <npraiwan@outlook.com>
2017-04-04 17:09:59 +0700

    Add fontCache

---
+++ library/src/main/java/com/dpizarro/uipicker/library/picker/FontCache.java
@@ -0,0 +1,25 @@
+package com.dpizarro.uipicker.library.picker;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+import java.util.Hashtable;
+
+class FontCache {
+
+    private static Hashtable<String, Typeface> fontCache = new Hashtable<String, Typeface>();
+
+    static Typeface get(String name, Context context) {
+        Typeface tf = fontCache.get(name);
+        if(tf == null) {
+            try {
+                tf = Typeface.createFromAsset(context.getAssets(), name);
+            }
+            catch (Exception e) {
+                return null;
+            }
+            fontCache.put(name, tf);
+        }
+        return tf;
+    }
+}
